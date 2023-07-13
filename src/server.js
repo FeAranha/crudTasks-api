cc695fd201512fdab1f3b0b468993fb0091b07b8
fearanha <felipearanha.c@gmail.com>
2023-07-13 15:01:57 -0300

    DB | middleware/json | create|read

--- src/server.js
+++ src/server.js
@@ -1,7 +1,35 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+const database = new Database
+
+const server = http.createServer(async(req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  if (method === 'GET' && url === '/tasks') {
+    const tasks =  database.select('tasks')
+
+    return res.end(JSON.stringify(tasks))
+  }
+
+  if (method === 'POST' && url === '/tasks') {
+    const { title, desc } = req.body
+    
+    const task = {
+      id: 1,
+      title,
+      desc,
+    }
+    database.insert('tasks', task)
+    
+    return res.writeHead(201).end()
+  }
+  return res
+    .writeHead(404)  
+    .end('Not Found')
 })
 
 server.listen(3333)
